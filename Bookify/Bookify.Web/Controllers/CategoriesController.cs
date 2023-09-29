48247f8bb791aec103e25bddcc51a318fb874880
yousef547 <54797102+yousef547@users.noreply.github.com>
2023-09-29 15:23:09 +0300

    Complate Section `Start Working With Categories Module`

---
+++ Bookify/Bookify.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,77 @@
+﻿using Bookify.Web.Core.Models;
+using Bookify.Web.Core.ViewModel;
+using Bookify.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Bookify.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var catogery = _context.Categories.ToList();
+            return View(catogery);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Form");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryFormViewModel model)
+        {
+            if(!ModelState.IsValid)
+                return View("Form",model);
+
+            var category = new Category { Name = model.Name };
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category is null)
+                return NotFound();
+
+            var viewModel = new CategoryFormViewModel
+            {
+                Id = id,
+                Name = category.Name
+            };
+
+            return View("Form", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Form", model);
+
+            var category = _context.Categories.Find(model.Id);
+
+            if (category is null)
+                return NotFound();
+
+            category.Name = model.Name;
+            category.LastCreatedOn = DateTime.Now;
+
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Bookify/Bookify.Web/Core/ViewModel/CategoryFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookify.Web.Core.ViewModel
+{
+    public class CategoryFormViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(100, ErrorMessage = "Max length cannot be more than 5 chr.")]
+        public string Name { get; set; }
+    }
+}
