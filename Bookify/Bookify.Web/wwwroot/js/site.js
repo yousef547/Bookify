3185891f1df2fae746784c57a7b87465a8193498
yousef547 <54797102+yousef547@users.noreply.github.com>
2023-10-01 15:41:44 +0300

    Complate Section `Working With DataTables`

--- Bookify/Bookify.Web/wwwroot/js/site.js
+++ Bookify/Bookify.Web/wwwroot/js/site.js
@@ -1,4 +1,8 @@
 ﻿var updatedRow;
+var datatable;
+var updatedRow;
+var exportedCols = [];
+
 function showSuccessMessage(message = 'Saved successfully!') {
     Swal.fire({
         icon: 'success',
@@ -10,17 +14,21 @@ function showSuccessMessage(message = 'Saved successfully!') {
     });
 }
 
+
+var headers = $('th');
+$.each(headers, function (i) {
+    if (!$(this).hasClass('js-no-export'))
+        exportedCols.push(i);
+});
 function onModalSuccess(item) {
     showSuccessMessage();
     $('#Modal').modal('hide');
-
-        $('tbody').append(item);
-    if (updatedRow === undefined) {
-    } else {
-        $(updatedRow).replaceWith(item);
+    if (updatedRow !== undefined) {
+        datatable.row(updatedRow).remove().draw();
         updatedRow = undefined;
     }
-
+    var newRow = $(item);
+    datatable.row.add(newRow).draw();
     KTMenu.init();
     KTMenu.initHandlers();
 }
@@ -36,13 +44,102 @@ function showErrorMessage(message = 'Something went wrong!') {
     });
 }
 
+var KTDatatables = function () {
+    // Private functions
+    var initDatatable = function () {
+        // Init datatable --- more info on datatables: https://datatables.net/manual/
+        datatable = $(table).DataTable({
+            "info": false,
+            'pageLength': 10,
+        });
+    }
+
+    // Hook export buttons
+    var exportButtons = () => {
+        const documentTitle = $('.js-datatables').data('document-title');
+        var buttons = new $.fn.dataTable.Buttons(table, {
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                },
+                {
+                    extend: 'csvHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    title: documentTitle,
+                    exportOptions: {
+                        columns: exportedCols
+                    }
+                }
+            ]
+        }).container().appendTo($('#kt_datatable_example_buttons'));
+
+        // Hook dropdown menu click event to datatable export buttons
+        const exportButtons = document.querySelectorAll('#kt_datatable_example_export_menu [data-kt-export]');
+        exportButtons.forEach(exportButton => {
+            exportButton.addEventListener('click', e => {
+                e.preventDefault();
+
+                // Get clicked export value
+                const exportValue = e.target.getAttribute('data-kt-export');
+                const target = document.querySelector('.dt-buttons .buttons-' + exportValue);
+
+                // Trigger click event on hidden datatable export buttons
+                target.click();
+            });
+        });
+    }
+
+    // Search Datatable --- official docs reference: https://datatables.net/reference/api/search()
+    var handleSearchDatatable = () => {
+        const filterSearch = document.querySelector('[data-kt-filter="search"]');
+        filterSearch.addEventListener('keyup', function (e) {
+            datatable.search(e.target.value).draw();
+        });
+    }
+
+    // Public methods
+    return {
+        init: function () {
+            table = document.querySelector('.js-datatables');
+
+            if (!table) {
+                return;
+            }
+
+            initDatatable();
+            exportButtons();
+            handleSearchDatatable();
+        }
+    };
+}();
 $(document).ready(function () {
+    //DataTables
+    KTUtil.onDOMContentLoaded(function () {
+        KTDatatables.init();
+    });
     var message = $('#Message').text();
     if (message !== '') {
         showSuccessMessage(message);
     }
 
-    $('body').delegate(".js-render-modal","click", function () {
+    $('body').delegate(".js-render-modal", "click", function () {
         var btn = $(this);
         var modal = $("#Modal");
         modal.find("#ModalLabel").text(btn.data('title'));
