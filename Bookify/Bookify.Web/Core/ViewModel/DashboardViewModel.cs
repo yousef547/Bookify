5dcb454606af3532ace9ddea5d9e3d793fd76bd9
yousef547 <54797102+yousef547@users.noreply.github.com>
2023-10-22 11:35:55 +0300

    Complate Section `Working on Dashboard`

---
+++ Bookify/Bookify.Web/Core/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Web.Core.ViewModel;
+
+namespace Bookify.Web.Core.ViewModel
+{
+    public class DashboardViewModel
+    {
+        public int NumberOfCopies { get; set; }
+        public int NumberOfSubscribers { get; set; }
+        public IEnumerable<BookViewModel> LastAddedBooks { get; set; } = new List<BookViewModel>();
+        public IEnumerable<BookViewModel> TopBooks { get; set; } = new List<BookViewModel>();
+    }
+}
